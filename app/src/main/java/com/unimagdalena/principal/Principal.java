9cfb85574cf3e59f6bc3dcb7ed35c49a18dd5c53
Andres Camargo <ingcamargolastra@gmail.com>
2019-07-12 21:59:39 -0500

    Pantalla inicial terminada

--- app/src/main/java/com/unimagdalena/principal/Principal.java
+++ app/src/main/java/com/unimagdalena/principal/Principal.java
@@ -1,13 +1,41 @@
 package com.unimagdalena.principal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Principal extends AppCompatActivity {
+
+    private ListView lista;
+    private String[] opciones;
+    private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lista = findViewById(R.id.lstOpciones);
+        opciones = getResources().getStringArray(R.array.opciones_lista);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
+        lista.setAdapter(adapter);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        i = new Intent(Principal.this, CrearPersonas.class);
+                        startActivity(i);
+                        break;
+                    case 1:
+                        i = new Intent(Principal.this, ListarPersonas.class);
+                        startActivity(i);
+                        break;
+                }
+            }
+        });
     }
 }
