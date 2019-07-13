6ca99d35166c506c3993d3e4e7efb8cc23749c5f
Andres Camargo <ingcamargolastra@gmail.com>
2019-07-13 09:12:01 -0500

    Guardar y limpiar terminado

--- app/src/main/java/com/unimagdalena/principal/CrearPersonas.java
+++ app/src/main/java/com/unimagdalena/principal/CrearPersonas.java
@@ -2,12 +2,34 @@ package com.unimagdalena.principal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CrearPersonas extends AppCompatActivity {
-
+    private EditText cedula, nombre, apellido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_personas);
+        cedula = findViewById(R.id.txtCedula);
+        nombre = findViewById(R.id.txtNombre);
+        apellido = findViewById(R.id.txtApellido);
+    }
+
+    public void guardar(View v){
+        String nom, ape, ced;
+        Persona p;
+        ced = cedula.getText().toString();
+        nom = nombre.getText().toString();
+        ape = apellido.getText().toString();
+        p = new Persona(ced,nom,ape);
+        p.guardar();
+    }
+
+    public void limpiar(View v){
+        cedula.setText("");
+        nombre.setText("");
+        apellido.setText("");
+        cedula.requestFocus();
     }
 }
